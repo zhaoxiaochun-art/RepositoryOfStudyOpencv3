9f2e692ff93bfd4ffd0132a771c87ef0fdc90c12
zhaoxiaochun-art <337416641@qq.com>
2020-07-30 17:08:29 +0800

    can be executed program

--- studyOpencv3/main.cpp
+++ studyOpencv3/main.cpp
@@ -1,10 +1,19 @@
 #include "studyOpencv3.h"
 #include <QtWidgets/QApplication>
+#include "opencv2/opencv.hpp"
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    studyOpencv3 w;
-    w.show();
-    return a.exec();
+	Mat img = imread("1233.jpg",IMREAD_REDUCED_COLOR_4);
+	if (img.empty()) return -1;
+	namedWindow("Example1", WINDOW_AUTOSIZE);
+	imshow("Example1", img);
+	waitKey(0);
+	destroyWindow("Example1");
+	return 0;
+	/*QApplication a(argc, argv);
+	studyOpencv3 w;
+	w.show();
+	return a.exec();*/
 }
